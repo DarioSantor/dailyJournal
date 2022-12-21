0b7efc05998e1503ea8f5a30d352694842dcc6aa
Dario Santos <dario.santos@hotmail.com>
2022-12-21 09:21:32 +0000

    refactor: change to no-storyboard

---
+++ Daily Journal/Daily Journal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Daily Journal
+//
+//  Created by Santos, Dario Ferreira on 21/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
